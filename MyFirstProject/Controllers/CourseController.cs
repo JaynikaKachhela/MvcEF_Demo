5b8d93d2c873ee89c4592a68f5b780cb00574851
JaynikaKachhela <jaynika@promactinfo.com>
2016-07-08 17:41:23 +0530

    Course

--- MyFirstProject/Controllers/CourseController.cs
+++ MyFirstProject/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,26 @@ namespace MyFirstProject.Controllers
 {
     public class CourseController : Controller
     {
+        private SchoolContext db = new SchoolContext();
         public ActionResult Index()
         {
+            return View(db.Courses.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Name,TotalCredits")] Course course)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(course);
         }
        
     }
